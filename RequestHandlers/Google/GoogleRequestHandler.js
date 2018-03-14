4eb29383456980d180ca3b57ad62e82ffd874074
gberk <gavin.berkowitz.bu@gmail.com>
2018-03-14 11:12:15 -0400

    Get user expression from Dialogflow when using synonyms

--- RequestHandlers/Google/GoogleRequestHandler.js
+++ RequestHandlers/Google/GoogleRequestHandler.js
@@ -4,9 +4,11 @@ var ExecuteIntent = require('../../IntentExecutor')
 var GoogleRequestParser = function(googleArgs, _res){
     //attach or create Device related to request
     var context = {}
-    context.args = googleArgs.result.parameters  || {};
+    context.args = googleArgs.result.contexts[0].parameters || googleArgs.result.parameters  || {};
+    
     context.intentName = googleArgs.result.action
     context.assistant = new GoogleAssistant(_res)
+    console.log(context)
     context.deviceProfile = {
         platform: "google",
         id: googleArgs.originalRequest.data.user.userId
